2667a2690a91b1f52a2235b1438b3477ded241f7
Logan Wood <lwood3499@gmail.com>
2019-09-08 21:54:52 -0500

    wip setting up haeder

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,19 @@
 <script>
-	export let name;
+  import Header from "./UI/Header.svelte";
+
+  const meetups = [
+    {
+      id: m1,
+      title: "Svelte, meet Dallas",
+      subtitle: "Creator of Svelte meets Dallas",
+      description:
+        "The creator of Svelte, Rich Harris, is giving a talk and introducing Dallas to this new, exciting tool.",
+      imageUrl:
+        "http://res.cloudinary.com/culturemap-com/image/upload/ar_4:3,c_fill,g_faces:center,w_1200/v1508855968/photos/263214_original.jpg",
+      address: "123 Main St, 12345 Dallas, TX",
+      contactEmail: "abc@123.com"
+    }
+  ];
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+<Header />
---
+++ src/styles/AppStyles.js
@@ -0,0 +1,9 @@
+import emotion from "emotion/dist/emotion.umd.min.js";
+
+const { css } = emotion;
+
+const skyBlue = "blue";
+
+export const headerTitle = css`
+	color: ${skyBlue};
+`;
